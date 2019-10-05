689f34ae324ee63953087a87c620f33eb6eef7f3
[Gayathri Buddhika] <[wg.gayathri@gmail.com]>
2019-10-05 10:45:34 +0530

    Modules in python
    Define Basis of Modules in python

---
+++ code.py
@@ -0,0 +1,12 @@
+def calculations(n1, n2):
+    sum = n1 + n2
+    sub = n1 - n2
+    mul = n1 * n2
+    div = n1 / n2
+    mod = n1 % n2
+
+    print("Sum = ", sum)
+    print("Sub = ", sub)
+    print("Mul = ", mul)
+    print("Div = ", div)
+    print("Mod = ", mod)
---
+++ main.py
@@ -0,0 +1,8 @@
+import code
+import random
+
+x = random.randrange(1, 100)
+print(x)
+print(" ")
+code.calculations(10, 3)
+
